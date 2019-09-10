0498b4dac7357824850727d878d1bd153afbb78b
nepoklonov <i.s.nepoklonov@yandex.ru>
2019-09-10 04:38:39 +0300

    improving get-up "join" page

--- client/src/main/kotlin/sample/pages/Join.kt
+++ client/src/main/kotlin/sample/pages/Join.kt
@@ -9,8 +9,9 @@ import kotlinx.html.js.onSubmitFunction
 import org.w3c.dom.HTMLInputElement
 import react.*
 import react.dom.RDOMBuilder
-import react.dom.form
+import react.dom.p
 import sample.gray50Color
+import sample.gray70Color
 import sample.stucture.PageState
 import sample.stucture.StandardPageComponent
 import styled.*
@@ -20,22 +21,22 @@ interface JoinState : PageState {
 //    var validMap: MutableMap<String, Boolean>
 }
 
-fun validateByPattern(sample: String, pattern: String) = Regex(pattern).matches(sample)
+fun validateByPattern(sample: String, pattern: String) = Regex(pattern, setOf(RegexOption.IGNORE_CASE)).matches(sample)
 
 val validateEmail: (String) -> Boolean = {
-    validateByPattern(it, "i.s.nepoklonov@yandex.ru")
+    validateByPattern(it, "(([^<>()[\\]\\.,;:\\s@\\\"]+(\\.[^<>()[\\]\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))@(([^<>()[\\]\\.,;:\\s@\\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\\"]{2,})")
 }
 
 val validateTypicalWords: (String) -> Boolean = {
-    validateByPattern(it, "i.s.nepoklonov@yandex.ru")
+    validateByPattern(it, "[^]+")
 }
 
 val validateNumber: (String) -> Boolean = {
-    validateByPattern(it, "i.s.nepoklonov@yandex.ru")
+    validateByPattern(it, "[1-9]\\d?")
 }
 
 val validateCheckBox: (String) -> Boolean = {
-    validateByPattern(it, "i.s.nepoklonov@yandex.ru")
+    false
 }
 
 interface InputItemProps : RProps {
@@ -43,6 +44,7 @@ interface InputItemProps : RProps {
     var name: String
     var title: String
     var validation: (String) -> Boolean
+    var width: LinearDimension
 }
 
 interface InputItemState : RState {
@@ -60,15 +62,21 @@ class InputItem : RComponent<InputItemProps, InputItemState>() {
         styledDiv {
             css {
                 display = Display.inlineFlex
-                flexDirection = when (props.type) {
-                    InputType.text -> FlexDirection.columnReverse
-                    InputType.checkBox -> FlexDirection.row
-                    else -> FlexDirection.column
+                width = props.width
+                if (props.type == InputType.text) {
+//                    minHeight = 45.px
+                    flexDirection = FlexDirection.columnReverse
+                    if (!state.isEmpty) {
+                        marginTop = 15.px
+                    }
+                } else if (props.type == InputType.checkBox) {
+                    marginTop = 30.px
+                    flexDirection = FlexDirection.row
+                } else {
+                    marginTop = 30.px
+                    flexDirection = FlexDirection.column
                 }
-                margin(0.px, 10.px, 5.px, 10.px)
-                if (state.isCorrect) {
-                    backgroundColor = Color.green
-                }
+
             }
             styledInput(type = props.type, name = props.name) {
                 attrs {
@@ -88,6 +96,9 @@ class InputItem : RComponent<InputItemProps, InputItemState>() {
                         borderBottom(1.px, BorderStyle.solid, gray50Color)
                         height = 20.px
                         zIndex = 2
+                        if (!state.isEmpty && state.isCorrect) {
+                            borderBottomColor = Color.green
+                        }
                     } else if (props.type == InputType.checkBox) {
                         marginRight = 5.px
                     }
@@ -96,8 +107,8 @@ class InputItem : RComponent<InputItemProps, InputItemState>() {
             }
             styledSpan {
                 css {
+                    position = Position.relative
                     if (props.type == InputType.text) {
-                        position = Position.relative
                         if (state.isEmpty) {
                             top = 20.px
                             fontSize = 16.px
@@ -105,6 +116,8 @@ class InputItem : RComponent<InputItemProps, InputItemState>() {
                             top = 2.px
                             fontSize = 12.px
                         }
+                    } else if (props.type == InputType.checkBox) {
+                        top = (-3).px
                     }
                     color = gray50Color
                 }
@@ -118,39 +131,53 @@ fun RDOMBuilder<*>.inputItem(
     type: InputType,
     name: String,
     title: String,
-    validation: (String) -> Boolean
+    validation: (String) -> Boolean = { false },
+    width: LinearDimension = 100.pct
 ) = child(InputItem::class) {
     attrs.also {
         it.type = type
         it.name = name
         it.title = title
         it.validation = validation
+        it.width = width
     }
 }
 
 class JoinComponent : StandardPageComponent<JoinState>() {
     override fun StyledDOMBuilder<*>.page() {
-        form(method = FormMethod.post) {
+        styledForm (action = "", method = FormMethod.post) {
+            styledP {
+                css {
+                    color = gray70Color
+                }
+                +"Чтобы принять участие в акции, заполните, пожалуйста, небольшую анкету:"
+            }
+            css {
+                margin(0.px, 30.px, 0.px, 30.px)
+                display = Display.flex
+                flexWrap = FlexWrap.wrap
+                justifyContent = JustifyContent.spaceBetween
+            }
             attrs.onSubmitFunction = {
                 window.alert()
             }
-            inputItem(InputType.text, "surname", "Фамилия", validateTypicalWords)
-            inputItem(InputType.text, "name", "Имя", validateTypicalWords)
-            inputItem(InputType.text, "age", "Возраст", validateNumber)
-            inputItem(InputType.text, "city", "Населённый пункт", validateTypicalWords)
-            inputItem(InputType.text, "school", "Образовательное учреждение", validateTypicalWords)
-            inputItem(InputType.text, "email", "E-Mail", validateEmail)
-            inputItem(InputType.checkBox, "supervisor", "Есть куратор/преподаватель", validateCheckBox)
-            inputItem(InputType.text, "supervisorFIO", "ФИО куратора/преподавателя", validateTypicalWords)
-            inputItem(InputType.text, "supervisorContacts", "E-mail или телефон куратора/преподавателя", validateTypicalWords)
-            inputItem(InputType.text, "title", "Название рисунка", validateTypicalWords)
-            inputItem(InputType.file, "file", "Загрузите рисунок", validateTypicalWords)
-            inputItem(InputType.text, "essayTitle", "Название эссе (по желанию)", validateTypicalWords)
-            inputItem(InputType.text, "essayText", "Текст эссе (по желанию)", validateTypicalWords)
-            inputItem(InputType.file, "essayFile", "Загрузите рисунок", validateTypicalWords)
-            inputItem(InputType.checkBox, "agree", "Я даю согласие на обработку моих персональных данных.\nОзнакомиться с требованиями федерального закона о персональных данных", validateCheckBox)
-            inputItem(InputType.checkBox, "know", "Я ознакомлен с Положением об Акции", validateCheckBox)
-            inputItem(InputType.submit, "submit", "Загрузить работу", validateCheckBox)
+            inputItem(InputType.text, "surname", "Фамилия", validation = validateTypicalWords, width = 35.pct)
+            inputItem(InputType.text, "name", "Имя", validation = validateTypicalWords, width = 25.pct)
+            inputItem(InputType.text, "age", "Возраст", validation = validateNumber, width = 17.pct)
+            inputItem(InputType.text, "city", "Населённый пункт", validation = validateTypicalWords)
+            inputItem(InputType.text, "school", "Образовательное учреждение", validation = validateTypicalWords)
+            inputItem(InputType.text, "email", "E-Mail", validation = validateEmail)
+            inputItem(InputType.checkBox, "supervisor", "Есть куратор/преподаватель", validation = validateCheckBox)
+            inputItem(InputType.text, "supervisorFIO", "ФИО куратора/преподавателя", validation = validateTypicalWords)
+            inputItem(InputType.text, "supervisorContacts", "E-mail или телефон куратора/преподавателя", validation = validateTypicalWords)
+            inputItem(InputType.text, "title", "Название рисунка", validation = validateTypicalWords, width = 40.pct)
+            inputItem(InputType.file, "file", "Загрузите рисунок", validation = validateCheckBox, width = 40.pct)
+            inputItem(InputType.text, "essayTitle", "Название эссе (по желанию)", validation = validateTypicalWords, width = 40.pct)
+            inputItem(InputType.text, "essayText", "Текст эссе (по желанию)", validation = validateTypicalWords)
+            inputItem(InputType.file, "essayFile", "Загрузите рисунок", validation = validateCheckBox)
+            inputItem(InputType.checkBox, "agree", "Я даю согласие на обработку моих персональных данных.\nОзнакомиться с требованиями федерального закона о персональных данных", validation = validateCheckBox)
+            inputItem(InputType.checkBox, "know", "Я ознакомлен с Положением об Акции", validation = validateCheckBox)
+            inputItem(InputType.submit, "submit", " ", validation = validateCheckBox, width = 40.pct)
         }
     }
 }
